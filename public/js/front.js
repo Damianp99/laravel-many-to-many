2cfd7b9f97a5a41159e069e59bdfd413d5f628c7
Damianp99 <damianperez200199@gmail.com>
2022-03-30 16:53:56 +0200

    compiled files

--- public/js/front.js
+++ public/js/front.js
@@ -49899,7 +49899,7 @@ var root = new Vue({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Laravel\laravel-auth\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Laravel\laravel-many-to-many\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
