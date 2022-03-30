2f5398a35dfebe70ee6051ba99e2791fb10fa94b
Damianp99 <damianperez200199@gmail.com>
2022-03-30 17:23:09 +0200

    added tables for tag and post_tag

---
+++ database/migrations/2022_03_30_151217_create_tag_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag', function (Blueprint $table) {
+            $table->id();
+            $table->string('label');
+            $table->char('color', 7)->nullable()->default('#ffffff');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag');
+    }
+}
