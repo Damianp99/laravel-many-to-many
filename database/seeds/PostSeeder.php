e3fce922e29381c699d35d70069d9e43878cd717
Damianp99 <damianperez200199@gmail.com>
2022-04-01 16:25:32 +0200

    added user id and tag id to posts

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Arr;
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\Tag;
+use App\User;
 use Faker\Generator as Faker;
 
 class PostSeeder extends Seeder
@@ -16,8 +18,12 @@ class PostSeeder extends Seeder
     public function run(Faker $faker)
     {
         $category_ids = Category::pluck('id')->toArray();
+        $user_ids = User::pluck('id')->toArray();
+        $tag_ids = Tag::pluck('id')->toArray();
         for ($i = 0; $i < 15; $i++) {
             $post = new Post();
+            $post->tag_id = Arr::random($tag_ids);
+            $post->user_id = Arr::random($user_ids);
             $post->category_id = Arr::random($category_ids);
             $post->title = $faker->text(20);
             $post->description = $faker->paragraphs(2, true);
