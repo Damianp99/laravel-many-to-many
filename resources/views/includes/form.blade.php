84f0d8d953a93b5f8c3b1e6d5c145027fbd9fccf
Damianp99 <damianperez200199@gmail.com>
2022-04-01 17:29:41 +0200

    added checkbox on form

--- resources/views/includes/form.blade.php
+++ resources/views/includes/form.blade.php
@@ -6,12 +6,13 @@
 <div class="form-group col-2">
     <label for="category">Categoria</label>
     <select class="form-control" id="category" name="category_id">
-      <option>Nessuna Categoria</option>
-      @foreach ($categories as $category)
-      <option value="{{ $category->id }}">
-        {{$category->label}}
-      </option>
-      @endforeach
+        @forelse ($categories as $category)
+            <option value="{{ $category->id }}">
+                {{$category->label}}
+            </option>
+        @empty
+            <option value="">Nessuna Categoria</option>
+        @endforelse
     </select>
 </div>
 <div class="form-group col-12">
@@ -19,10 +20,21 @@
     <input type="url" class="form-control" id="image" name="image" placeholder="Immagine" value="{{$post->image}}">
 </div>
 <div class="form-group col-12">
-    <label for="description">Example textarea</label>
+    <label for="description">Descrizione </label>
     <textarea class="form-control" id="description" name="description" rows="3">{{$post->description}}</textarea>
 </div>
-<div class="col-12 d-flex justify-content-end align-items-center">
+
+{{-- check box and button --}}
+<div class="col-12 d-flex justify-content-between align-items-center">
+    <div class="d-flex align-items-center justify-content-center">
+        @foreach ($tags as $tag)
+        <div class="form-check mx-3">
+            <input class="form-check-input" type="checkbox" value="{{ $tag->id }}"
+             id="tag-{{ $tag->id }}" name="tags[]" @if (in_array($tag->id, old('tags', $post_tag_ids ?? []))) checked @endif>
+            <label class="form-check-label" for="tag-{{ $tag->id }}">{{ $tag->label }}</label>
+        </div>
+        @endforeach
+    </div>
     <button type="submit" class="btn btn-sm btn-success">Pubblica</button>
 </div>
 </div>
