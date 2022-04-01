04e0001894dd6f4021ab70dfb327ae0d1ddcd46b
Damianp99 <damianperez200199@gmail.com>
2022-04-01 17:29:16 +0200

    added tags on show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,6 +3,11 @@
 <div class="card container">
     <h1>{{$post->title}}</h1>
     <p>{{$post->description}}</p>
+    @forelse ($post->tags as $tag)
+        <p class="bagde rounded py-1 px-2 mx-2 text-center"
+        style="background-color: {{ $tag->color }}">{{ $tag->label }}</p>
+    @empty <p>Nessun tag</p>
+    @endforelse
     <img src="{{$post->image}}" alt="no-image">
     <div class="d-flex align-items-center justify-content-between py-2">
         <span>{{$post->created_at}}</span>
